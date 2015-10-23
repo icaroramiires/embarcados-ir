28320495b8d7114643a7f083355e12d18fca3586
igoromero <igo_2007@hotmail.com>
2015-10-23 13:23:20 -0300

    Criacao dos Worskpace de trabalho c++ e java

--- barramento/sketchbarramento/sketchbarramento.ino
+++ barramento/sketchbarramento/sketchbarramento.ino
@@ -1,1 +1,28 @@
+#include <Wire.h>
+#include "ADXL345.h"
 
+ADXL345 acelerometro = ADXL345();
+
+void setup(){
+
+  Serial.begin(9600);
+  acelerometro.powerOn();
+  
+}
+
+void verificarTerremoto() {
+bool x;
+
+
+x = acelerometro.getActivityThreshold();
+
+Serial.println(x);
+
+}
+
+
+void loop(){
+  
+  verificarTerremoto();
+
+}
