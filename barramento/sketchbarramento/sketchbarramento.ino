3bea1f8d0e6585be146dbf798d737917165108d1
igoromero <igo_2007@hotmail.com>
2015-10-24 12:28:30 -0300

    Alteracao nos metodos para capturar as informações e criação do metodo para enviar as informações por um Buffer

--- barramento/sketchbarramento/sketchbarramento.ino
+++ barramento/sketchbarramento/sketchbarramento.ino
@@ -2,50 +2,64 @@
 #include "ADXL345.h"
 #include "Adafruit_BMP085.h"
 
- int x,y,z; 
+struct Dados{ //Estutura contendo as informaçoes que seram enviadas no buffer
+  bool atividade;
+  int altitude;
+};
 
-ADXL345 adxl;
-Adafruit_BMP085 bmp;
+Dados dados;
+
+ADXL345 acelerometro = ADXL345(); // Criando o acelerometro
+
+Adafruit_BMP085 bmp = Adafruit_BMP085(); //Criando o Barometro 
 
 
 void setup(){
-
- Serial.begin(9600);
-   adxl.powerOn(); 
-   bmp.begin();
+ Serial.begin(9600); // Iniciando a comunicaçao Serial
+ acelerometro.powerOn(); // Iniciando acelerometro
+ bmp.begin();  // iniciando o Barometro 
 }
 
 void loop(){
   
- adxl.readAccel(&x, &y, &z);
+ byte leitura = acelerometro.getInterruptSource(); 
  
- byte leitura = adxl.getInterruptSource();
-
- verificaTerremoto(leitura);
- verificaAltitude();
-
+ dados.atividade = verificaTerremoto(leitura);
+ dados.altitude =  verificaAltitude();
+ enviarDados();
+ 
+ delay(100);
 }
 
+void enviarDados(){
 
-void verificaTerremoto(byte leitura){
+int tam = sizeof(dados);
+char buffer[tam];
+memcpy(&buffer,&dados,5);
 
-adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,   ADXL345_INT1_PIN );
+Serial.write('I');
+Serial.write((uint8_t*)&buffer, 5);
+Serial.write('F');
+}
+
+bool verificaTerremoto(byte leitura){
+acelerometro.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,   ADXL345_INT1_PIN );
  
 bool valor = ((leitura >> ADXL345_INT_ACTIVITY_BIT  ) & 1);
 
-if(valor == 1 ){
-  
-  Serial.println("atividade");
+if(valor == 1 ){  
+ return true;
  
-}
+}if(valor == 0){
+ return false;
 }
 
-void verificaAltitude(){
-  
-  Serial.print(" Altitude ");
-  Serial.print((bmp.readAltitude() + bmp.readAltitude(102200)) / 2 ); // altitude media = altitude padrao + 102200 Pascal <-  100 * 1022 milibar(altitude de Vitoria da Conquista) 
-  Serial.println(" metros");
 
+}
+
+int verificaAltitude(){
+  int altitude = (int) ((bmp.readAltitude() + bmp.readAltitude(102200)) / 2 ); // altitude media = altitude padrao + 102200 Pascal(altitude vca) <-  100 * 1022 milibar (pressao atimosferica VCA)  
+  return altitude;
 }
 
   
