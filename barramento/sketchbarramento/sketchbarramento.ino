ba19348a7bc9f76a4cd2c98f7158193c70a153df
igoromero <igo_2007@hotmail.com>
2015-10-24 10:50:32 -0300

    Inclusão da Biblioteca BMP085, implementacao do metodo de verificacao da Altitude

--- barramento/sketchbarramento/sketchbarramento.ino
+++ barramento/sketchbarramento/sketchbarramento.ino
@@ -1,40 +1,52 @@
-
-
 #include <Wire.h>
 #include "ADXL345.h"
+#include "Adafruit_BMP085.h"
 
  int x,y,z; 
 
 ADXL345 adxl;
+Adafruit_BMP085 bmp;
+
+
 void setup(){
 
  Serial.begin(9600);
    adxl.powerOn(); 
+   bmp.begin();
 }
 
 void loop(){
   
- adxl.readAccel(&x, &y, &z); 
+ adxl.readAccel(&x, &y, &z);
  
  byte leitura = adxl.getInterruptSource();
 
  verificaTerremoto(leitura);
-
+ verificaAltitude();
 
 }
 
 
 void verificaTerremoto(byte leitura){
 
- if(adxl.triggered(leitura, ADXL345_INACTIVITY)){
-  Serial.println("Sem terremoto");
- }
+adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+bool valor = ((leitura >> ADXL345_INT_ACTIVITY_BIT  ) & 1);
+
+if(valor == 1 ){
   
- if(adxl.triggered(leitura, ADXL345_ACTIVITY)){
-  Serial.println("Terremto Detectado"); 
-  
- }
+  Serial.println("atividade");
+ 
 }
+}
+
+void verificaAltitude(){
   
+  Serial.print(" Altitude ");
+  Serial.print((bmp.readAltitude() + bmp.readAltitude(102200)) / 2 ); // altitude media = altitude padrao + 102200 Pascal <-  100 * 1022 milibar(altitude de Vitoria da Conquista) 
+  Serial.println(" metros");
+
+}
+
   
   
